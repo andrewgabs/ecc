f104efa3ce1179bfba1577d8ef2f07cce18ee9c3
haccel <haccel@bitmarket.network>
2021-01-10 18:45:08 +0300

    -

--- scripts/mysig.py
+++ scripts/mysig.py
@@ -1,32 +1,56 @@
 import random
 
-N = 23
-G = 103
+N = 337
+G = 2477
 Priv = 73
 Pub = Priv * G
+
+koaf = 1
+
+
+def mul(a, b):
+    return (a * b) % N
+
+
+def div(a, b):
+    return int(a / b) % N
 
 
 def sign(z):
     r, s = 0, 0
     while not r or not s:
-        k = random.randrange(1, N-1)
-        x = k * G
-        r = x % N
-        s = int((z + Priv * r) / k) % N
+        global koaf
+        koaf = random.randrange(1, N-1)
+        print(f"K:{koaf}")
+        r = koaf * G
+        s = (z + Priv * r) / koaf
     return r, s
 
 
 def ver(z, r, s):
-    u1 = int(z / s) % N
-    u2 = int(r / s) % N
+    u1 = z / s
+    u2 = r / s
     print(f"u1:{u1} u2:{u2}")
-    P = u1 * G + u2 * Pub
-    return P % N
+    P = int(u1 * G + u2 * Pub)
+    print(f"P:{P}")
+    assert koaf == r / G, r/G
+
+    p1 = int(z * G / s + r * Priv * G / s)
+    assert P == p1, p1
+
+    p2 = int((z + r * Priv) / s * G)
+    assert P == p2, p2
+
+    p3 = int(koaf * G)
+    assert P == p3, p3
+
+    return P
 
 
-hash = 123
+hash = 1234343
 r, s = sign(hash)
 print(f"r/s: {r} : {s}")
 
 p = ver(hash, r, s)
 print(f"ver: {p}")
+print(f"good: {p == r}")
